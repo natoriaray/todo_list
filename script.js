89ed59a8843207fb2f0156a5afef8bc9042dd6a8
natoriaray <natoriaray@gmail.com>
2019-05-15 15:06:34 -0500

    created function to get input value from the input field

--- script.js
+++ script.js
@@ -1,20 +1,41 @@
-var UIController = (function() {
+var dataContoller = (function() {
+
+	
 
 
 })();
 
-var controller = (function(UTCtrl) {
 
-	var getInputValue = function() {
-		document.querySelector('.add_btn').addEventListener('click', function() {
-			;
-		});
 
-};
+var UIController = (function() {
+
+	return {
+		getInputValue: function() {
+			var inputDescription = document.querySelector('.input_text').value;
+			return inputDescription;
+		}
+	}
+
+})();
+
+var controller = (function(UICtrl) {
+
+	var eventListener = function() {
+		document.querySelector('.add_btn').addEventListener('click', addToDo)
+
+	};
+
+	var addToDo = function() {
+		var toDoItem
+
+		// 1. Get input fields
+		toDoItem = UICtrl.getInputValue();
+		console.log(toDoItem);
+	}
 
 	return {
 		init: function() {
-			getInputValue();
+			eventListener();
 		}
 	}
 
