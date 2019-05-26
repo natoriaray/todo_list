7d600d65e16cc3e145f7c157245116557e3eeefb
Natoria Ray <natoriaray@gmail.com>
2019-05-26 16:13:52 -0500

    removed jQuery and create new delete functions

--- script.js
+++ script.js
@@ -27,6 +27,20 @@ var dataContoller = (function() {
 
 			//return to do item
 			return newItem;
+		},
+
+		deleteToDoItem: function(id) {
+			var ids, index;
+
+			ids = allToDoItems.map(function(current) {
+				return current.id;
+			});
+
+			index = ids.indexOf(id);
+
+			if (index !== -1) {
+          allToDoItems.splice(index, 1);
+      }
 		}
 	}
 
@@ -43,11 +57,11 @@ var UIController = (function() {
 			return inputDescription;
 		},
 
-		displayToDoItem: function(item) {
+		displayItem: function(item) {
 			var html, newHTML;
 
 			//1. Create HTML string with placeholder text
-			html = '<div class="list_item" id="item-%id%"><div class="item_description">%description%</div><div class="delete_item"><button class="delete_btn"type="button" name="button"><ion-icon name="close"></ion-icon></button></div>'
+			html = '<div class="list_item" id="%id%"><div class="item_description">%description%</div><div class="delete_item"><button class="delete_btn"type="button" name="button"><ion-icon name="close"></ion-icon></button></div>'
 
 			//2. Replace placeholder text with the actual to do item
 			newHTML = html.replace('%id%', item.id)
@@ -63,11 +77,9 @@ var UIController = (function() {
 			inputField.focus();
 		},
 
-		deleteToDoItem: function() {
-			document.querySelector('.delete_btn').addEventListener('click', function() {
-				var element = document.querySelector('.list_item');
-				element.parentNode.removeChild(element);
-			})
+		deleteItem: function(selectorID) {
+			var el = document.getElementById(selectorID);
+			el.parentNode.removeChild(el);
 		},
 
 	}
@@ -89,12 +101,14 @@ var controller = (function(UICtrl, dataCtrl) {
 	};
 
 	var deleteToDo = function() {
-		console.log(event.target)
+		var itemID, ID;
 
+		itemID = event.target.parentNode.parentNode.parentNode.id;
 
 		//1.delete to do item from data structure
-
+		dataCtrl.deleteToDoItem(itemID);
 		//2.Delete to do item from UI
+		UICtrl.deleteItem(itemID);
 	}
 
 	var addToDo = function() {
@@ -108,15 +122,11 @@ var controller = (function(UICtrl, dataCtrl) {
 			newItem = dataCtrl.addToDoItem(toDoItemDes);
 			console.log(newItem.id)
 			// 3. Add to do item to the UI
-			UICtrl.displayToDoItem(newItem);
+			UICtrl.displayItem(newItem);
 
 			//4. Clear input field
 			UICtrl.clearField();
-
-
 		}
-
-
 	};
 
 	return {
